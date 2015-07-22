12339665d59a62af9689fdc7e9a8d16e955487e9
huxiaoqian <huxq17@163.com>
2015-07-22 16:58:21 +0800

    add circle's style

--- XCircleIndicator/src/com/andwidget/xcircleindicator/XCircleIndicator.java
+++ XCircleIndicator/src/com/andwidget/xcircleindicator/XCircleIndicator.java
@@ -3,8 +3,12 @@ package com.andwidget.xcircleindicator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Paint.FontMetrics;
 import android.graphics.Paint.Style;
+import android.text.Layout;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -22,22 +26,19 @@ import android.view.View;
  * <ul>
  * radius: Define the circle radius (default to 4)
  * </ul>
- * <ul>
- * circleInterval: Define the circle interval between circles(default to 4)
- * </ul>
- * <ul>
- * pageTotalCount: Define the page total count
- * </ul>
  */
 public class XCircleIndicator extends View {
 	private int radius = 4;
 	private final Paint mPaintStroke = new Paint(Paint.ANTI_ALIAS_FLAG);
 	private final Paint mPaintFill = new Paint(Paint.ANTI_ALIAS_FLAG);
+	private final TextPaint mPaintText = new TextPaint(Paint.ANTI_ALIAS_FLAG);
 	private int currentScroll = 0;
 	private int flowWidth = 0;
-	private int pageTotalCount = 1;
+	private int count = 1;
 	private int currentPage = 0;
 	private int circleInterval = radius;
+	private float mDensity;
+	private int upLimit = 6;
 
 	public XCircleIndicator(Context context) {
 		super(context);
@@ -58,16 +59,16 @@ public class XCircleIndicator extends View {
 
 		try {
 			// Retrieve the colors to be used for this view and apply them.
-			int fillColor = a.getColor(R.styleable.XCircleIndicator_fillColor,
-					0xFFFFFFFF);
+			int fillColor = a.getColor(
+					R.styleable.XCircleIndicator_fillColor, 0xFFFFFFFF);
 			int strokeColor = a.getColor(
 					R.styleable.XCircleIndicator_strokeColor, 0xFFFFFFFF);
 			// Retrieve the radius
-			radius = (int) a.getDimension(R.styleable.XCircleIndicator_radius,
-					radius);
+			radius = (int) a.getDimension(
+					R.styleable.XCircleIndicator_radius, radius);
 			circleInterval = (int) a.getDimension(
 					R.styleable.XCircleIndicator_circleInterval, radius);
-
+			mDensity = getContext().getResources().getDisplayMetrics().density;
 			initColors(fillColor, strokeColor);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -78,46 +79,145 @@ public class XCircleIndicator extends View {
 	}
 
 	/**
-	 * init page count and some params that maybe use in the future
 	 * 
-	 * @param count
-	 *            the total page count
-	 * @param contentWidth
-	 *            pass any value and we are not care now
+	 * @param upLimit
+	 *            default 4
 	 */
+	public void setUpLimit(int upLimit) {
+		this.upLimit = upLimit;
+	}
+
 	public void initData(int count, int contentWidth) {
-		this.pageTotalCount = count;
+		this.count = count;
 		this.flowWidth = contentWidth;
 		invalidate();
 	}
 
-	/**
-	 * set the current selected circleIndicator
-	 * 
-	 * @param currentPage
-	 *            current page number,start with zero
-	 */
 	public void setCurrentPage(int currentPage) {
 		this.currentPage = currentPage;
 		invalidate();
 	}
 
-	/**
-	 * Sets the circle interval
-	 * 
-	 * @param circleInterval
-	 *            unit px
-	 */
-	public void setPageTotalCount(int pageTotalCount) {
-		this.pageTotalCount = pageTotalCount;
-		invalidate();
-	}
+	private int mTextHeight;
 
 	private void initColors(int fillColor, int strokeColor) {
 		mPaintStroke.setStyle(Style.STROKE);
 		mPaintStroke.setColor(strokeColor);
 		mPaintFill.setStyle(Style.FILL);
 		mPaintFill.setColor(fillColor);
+		mPaintText.setColor(Color.parseColor("#F96A0E"));
+		mPaintText.setTextSize(18 * mDensity);
+		FontMetrics fm = mPaintText.getFontMetrics();
+		mTextHeight = (int) Math.ceil(fm.descent - fm.top) + 2;
+		;
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int left, int top, int right,
+			int bottom) {
+		super.onLayout(changed, left, top, right, bottom);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.view.View#onDraw(android.graphics.Canvas)
+	 */
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		// Draw stroked circles
+		if (count <= upLimit) {
+			for (int iLoop = 0; iLoop < count; iLoop++) {
+				canvas.drawCircle(getPaddingLeft() + radius
+						+ (iLoop * (2 * radius + circleInterval)),
+						getPaddingTop() + radius, radius, mPaintStroke);
+			}
+			int cx = 0;
+			// if (flowWidth != 0) {
+			// // Draw the filled circle according to the current scroll
+			// cx = (currentScroll * (2 * radius + radius)) / flowWidth;
+			// }
+			cx = (2 * radius + circleInterval) * currentPage;
+			// The flow width has been upadated yet. Draw the default position
+			canvas.drawCircle(getPaddingLeft() + radius + cx, getPaddingTop()
+					+ radius, radius, mPaintFill);
+		} else {
+			float textWidth = Layout.getDesiredWidth(currentPage + "/"
+					+ (count - 1), mPaintText);
+			canvas.drawText(currentPage + 1 + "/" + (count),
+					(getMeasuredWidth() - textWidth) / 2, getPaddingTop()
+							+ getMeasuredHeight(), mPaintText);
+		}
+	}
+
+	/**
+	 * Determines the width of this view
+	 * 
+	 * @param measureSpec
+	 *            A measureSpec packed into an int
+	 * @return The width of the view, honoring constraints from measureSpec
+	 */
+	private int measureWidth(int measureSpec) {
+		int result = 0;
+		int specMode = MeasureSpec.getMode(measureSpec);
+		int specSize = MeasureSpec.getSize(measureSpec);
+
+		// We were told how big to be
+		if (specMode == MeasureSpec.EXACTLY) {
+			result = specSize;
+		} else { // Calculate the width according the views count
+			result = getPaddingLeft() + getPaddingRight()
+					+ (count * 2 * radius) + (count - 1) * circleInterval;
+			// Respect AT_MOST value if that was what is called for by
+			// measureSpec
+			if (specMode == MeasureSpec.AT_MOST) {
+				result = Math.min(result, specSize);
+			}
+		}
+		return result;
+	}
+
+	public void onScrolled(int h, int v, int oldh, int oldv) {
+		currentScroll = h;
+		invalidate();
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		setMeasuredDimension(measureWidth(widthMeasureSpec),
+				measureHeight(heightMeasureSpec));
+	}
+
+	/**
+	 * Determines the height of this view
+	 * 
+	 * @param measureSpec
+	 *            A measureSpec packed into an int
+	 * @return The height of the view, honoring constraints from measureSpec
+	 */
+	private int measureHeight(int measureSpec) {
+		int result = 0;
+		int specMode = MeasureSpec.getMode(measureSpec);
+		int specSize = MeasureSpec.getSize(measureSpec);
+		if (count > upLimit) {
+			return mTextHeight + getPaddingTop() + getPaddingBottom();
+		}
+
+		// We were told how big to be
+		if (specMode == MeasureSpec.EXACTLY) {
+			result = specSize;
+		}
+		// Measure the height
+		else {
+			result = 2 * radius + getPaddingTop() + getPaddingBottom();
+			// Respect AT_MOST value if that was what is called for by
+			// measureSpec
+			if (specMode == MeasureSpec.AT_MOST) {
+				result = Math.min(result, specSize);
+			}
+		}
+		return result;
 	}
 
 	/**
@@ -142,112 +242,4 @@ public class XCircleIndicator extends View {
 		invalidate();
 	}
 
-	/**
-	 * Sets the circle interval
-	 * 
-	 * @param circleInterval
-	 *            unit px
-	 */
-	public void setCircleInterval(int circleInterval) {
-		this.circleInterval = circleInterval;
-		invalidate();
-	}
-
-	/**
-	 * Sets the circle radius
-	 * 
-	 * @param circleInterval
-	 *            unit px
-	 */
-	public void setRadius(int radius) {
-		this.radius = radius;
-		invalidate();
-	}
-
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		setMeasuredDimension(measureWidth(widthMeasureSpec),
-				measureHeight(heightMeasureSpec));
-	}
-
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		// Draw stroked circles
-		for (int iLoop = 0; iLoop < pageTotalCount; iLoop++) {
-			canvas.drawCircle(getPaddingLeft() + radius
-					+ (iLoop * (2 * radius + circleInterval)), getPaddingTop()
-					+ radius, radius, mPaintStroke);
-		}
-		int cx = 0;
-		// if (flowWidth != 0) {
-		// // Draw the filled circle according to the current scroll
-		// cx = (currentScroll * (2 * radius + radius)) / flowWidth;
-		// }
-		cx = (2 * radius + circleInterval) * currentPage;
-		// The flow width has been upadated yet. Draw the default position
-		canvas.drawCircle(getPaddingLeft() + radius + cx, getPaddingTop()
-				+ radius, radius, mPaintFill);
-	}
-
-	/**
-	 * Determines the width of this view
-	 * 
-	 * @param measureSpec
-	 *            A measureSpec packed into an int
-	 * @return The width of the view, honoring constraints from measureSpec
-	 */
-	private int measureWidth(int measureSpec) {
-		int result = 0;
-		int specMode = MeasureSpec.getMode(measureSpec);
-		int specSize = MeasureSpec.getSize(measureSpec);
-
-		// We were told how big to be
-		if (specMode == MeasureSpec.EXACTLY) {
-			result = specSize;
-		} else { // Calculate the width according the views count
-			result = getPaddingLeft() + getPaddingRight()
-					+ (pageTotalCount * 2 * radius) + (pageTotalCount - 1)
-					* circleInterval;
-			// Respect AT_MOST value if that was what is called for by
-			// measureSpec
-			if (specMode == MeasureSpec.AT_MOST) {
-				result = Math.min(result, specSize);
-			}
-		}
-		return result;
-	}
-
-	/**
-	 * Determines the height of this view
-	 * 
-	 * @param measureSpec
-	 *            A measureSpec packed into an int
-	 * @return The height of the view, honoring constraints from measureSpec
-	 */
-	private int measureHeight(int measureSpec) {
-		int result = 0;
-		int specMode = MeasureSpec.getMode(measureSpec);
-		int specSize = MeasureSpec.getSize(measureSpec);
-
-		// We were told how big to be
-		if (specMode == MeasureSpec.EXACTLY) {
-			result = specSize;
-		}
-		// Measure the height
-		else {
-			result = 2 * radius + getPaddingTop() + getPaddingBottom();
-			// Respect AT_MOST value if that was what is called for by
-			// measureSpec
-			if (specMode == MeasureSpec.AT_MOST) {
-				result = Math.min(result, specSize);
-			}
-		}
-		return result;
-	}
-
-	public void onScrolled(int h, int v, int oldh, int oldv) {
-		currentScroll = h;
-		invalidate();
-	}
 }
